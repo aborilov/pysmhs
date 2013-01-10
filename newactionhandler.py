cb8611c4286d6eac803be845c893021d30a48712
aborilov <aborilov@gmail.com>
2013-01-10 17:24:09 +0400

    read each tag in events, dont read prev tag in event

--- newactionhandler.py
+++ newactionhandler.py
@@ -10,24 +10,29 @@ class newactionhandler(AbstractHandler):
         AbstractHandler.__init__(self, parent, params)
         self.actions = {'setter': self.setter, 'switcher': self.switcher}
         self.actionlist = {}
+        self.temp_tags = {}
         for name, params in self.config.items():
             self.actionlist[params['condition']] = params['actions']
 
     def process(self, signal, events):
-        #fix dont ask for tag value that in events
-        self.logger.debug(events)
         for event in events:
-            tag, value = event.values()
-            for cond in [x for x in self.actionlist.keys() if tag in x]:
-                self.logger.debug(eval(cond))
-                try:
-                    if eval(cond):
-                        for i in sorted(self.actionlist[cond]):
-                            action = self.actionlist[cond][i].pop('action')
-                            params = self.actionlist[cond][i]
-                            self.actions.get(action, None)(params)
-                except:
-                    self.logger.error("Error while eval condition - %s" % cond)
+            self.temp_tags[event['tag']] = event['value']
+            for cond in self.actionlist.keys():
+                if event['tag'] in cond:
+                    try:
+                        if eval(cond):
+                            for i in sorted(self.actionlist[cond]):
+                                action = self.actionlist[cond][i].pop('action')
+                                params = self.actionlist[cond][i]
+                                self.actions.get(action, None)(params)
+                    except:
+                        self.logger.error("Error while eval(%s)" % cond)
+        self.temp_tags = {}
+
+    def gettag(self, tag):
+        if tag in self.temp_tags:
+            return self.temp_tags[tag]
+        return AbstractHandler.gettag(tag)
 
     def loadtags(self):
         for tag in self.config:
